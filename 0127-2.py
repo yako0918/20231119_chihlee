8a09b7a5505440fa2855ce8b3ca7c7825b5601f4
yako0918 <yako0918124051@gmail.com>
2024-01-27 13:28:22 +0800

    0127

---
+++ 0127-2.py
@@ -0,0 +1,29 @@
+from machine import Timer,Pin
+from machine import ADC, Pin
+import utime
+#
+# ADCs: GP26, GP27, GP28 and GP29-to VSYS
+#       ADC0, ADC1, ADC2 and ADC3 ，ADC4道連接內建溫度感測器，可用於測量溫度。
+#
+#
+adc_light_GP26 = ADC(Pin(26)) #ADC0
+adc_light_GP27 = ADC(Pin(27)) #ADC1
+adc_light_GP28 = ADC(Pin(28)) #ADC2
+adc_light_GP29 = ADC(Pin(29)) #ADC2
+
+light_value_26 = adc_light_GP26.read_u16()
+light_value_27 = adc_light_GP27.read_u16()
+light_value_28 = adc_light_GP28.read_u16()
+light_value_29 = adc_light_GP29.read_u16()
+
+def fun10(t:Time):
+    #print('1秒ㄌ')
+    #print(f"{abc}")
+    print(light_value_26)
+    print(light_value_27)
+    print(light_value_28)
+    print(light_value_29)
+    print('-------------------------')
+    pass
+
+timer10 = Timer(period=1000, mode=Timer.PERIODIC, callback=fun10 )
--- 0127.py
+++ 0127.py
@@ -1,2 +1,13 @@
 from machine import Timer,Pin
 
+def fun10(t:Time):
+    print('10秒ㄌ')
+    pass
+
+timer10 = Timer(period=10000, mode=Timer.PERIODIC, callback=fun10 )
+
+#t1 = t1+1
+
+
+#timer10 = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))執行一次
+#timer11 = tim.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:print(2))連續執行
