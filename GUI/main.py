ae64b51a2f49401d29bed3a1a393e206ffbaa9c1
yako0918 <yako0918124051@gmail.com>
2024-02-03 07:59:22 +0000

    02003

--- GUI/main.py
+++ GUI/main.py
@@ -2,12 +2,10 @@ import streamlit as st
 import pandas as pd
 import requests
 from streamlit_autorefresh import st_autorefresh
-import dotenv
+from dotenv import load_dotenv
 import os
 
-#streamlit run main.py 重啟服務
-
-dotenv.load_dotenv
+load_dotenv('.env')
 
 st_autorefresh(interval=5000)
 
@@ -15,7 +13,8 @@ st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.divider()
 
-url = f 'https://blynk.cloud/external/api/get?token={os.environ["API_KEY"]}&v0&v1'
+
+url = f'https://blynk.cloud/external/api/get?token={os.environ["API_KEY"]}&v0&v1'
 
 response = requests.request("GET",url)
 if response.status_code == 200:
