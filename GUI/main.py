9ad9e8a27d45daeb97d9c21adde74e0ecdd0e917
yako0918 <yako0918124051@gmail.com>
2024-02-03 07:58:10 +0000

    0203

--- GUI/main.py
+++ GUI/main.py
@@ -15,7 +15,7 @@ st.title("Pico_W_職能發展協會專案")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
 st.divider()
 
-url =(f 'https://blynk.cloud/external/api/get?token=(os.environ["API_KEY"])')
+url = f 'https://blynk.cloud/external/api/get?token={os.environ["API_KEY"]}&v0&v1'
 
 response = requests.request("GET",url)
 if response.status_code == 200:
