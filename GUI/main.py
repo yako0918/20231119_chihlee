d97ec4ecdaf9c85d8fe0e6457b1c2c98380a1798
yako0918 <yako0918124051@gmail.com>
2024-02-03 03:47:04 +0000

    0203

--- GUI/main.py
+++ GUI/main.py
@@ -3,4 +3,7 @@ import pandas as pd
 
 st.title("XXXX --QQQAAAXXX  Pico_W 0203專案建置")
 st.header("雞舍:red[溫度]和:blue[光線]狀態")
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
+st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
 st.divider()
